88b8fa6fe4c661385bc4118ae8306e558b145f4c
Arlind3553 <georgetesco@yahoo.com>
2023-07-25 07:11:32 -0700

    task 1

--- _printf.c
+++ _printf.c
@@ -1,31 +1,41 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-int print_char (va_list char_li);
+
+int print_char(va_list cha)
 {
-	char ch = va_arg(char_li, int);
-	putchar(ch);
+	char ch;
+
+	ch = va_arg(cha, int);
+	_putchar(ch);
 	return (1);
 }
-int print_string(va_list string_li)
+
+int print_string(va_list string)
 {
 	char *str;
-	int i;
+	int i = 0;
 
-	str = va_arg(string_li, char*);
-	for(i = 0; str[i] != '/0'; i++)
-		putchar(str[i]);
+	str = va_arg(string, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	return (i);
-
 }
+
 int _printf(const char *format, ...)
 {
 	int i = 0, j, count = 0;
 	va_list arg_list;
-	p_struct print_var[] = {{"c", print_char}, {"s", print_string},
-	{"i", change_int}, {"d", change_int}, {NULL, NULL}};
+	p_struct print_var[] = {
+		{"c", print_char}, 
+		{"s", print_string},
+		{NULL, NULL}};
 
 	if (format == NULL)
 		return (-1);
--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <stdef.h>
+#include <stddef.h>
 /**
  *struct print_struct - print_struct
  *@f: identifier of the function
@@ -15,10 +15,10 @@ typedef struct print_struct
 } p_struct;
 
 int _putchar (char c);
-int print_char (va_list char_li);
-int print_string(va_list string_li);
-int print_int (va_list int_li);
+int print_char (va_list cha);
+int print_string(va_list string);
+int print_int (va_list arg_list);
 int _printf(const char *format, ...);
 
 
-#endif MAIN_H
+#endif
