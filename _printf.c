a3a332d37479f45bfda6ccf1169b1f860231e8f6
Holberton1990 <ersida9019@gmail.com>
2023-07-25 14:00:32 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -6,17 +6,20 @@
 int print_char (va_list char_li);
 {
 	char ch = va_arg(char_li, int);
-	putchar(ch);
+
+	putchar (ch);
+
 	return (1);
 }
+
 int print_string(va_list string_li)
 {
 	char *str;
 	int i;
 
 	str = va_arg(string_li, char*);
-	for(i = 0; str[i] != '/0'; i++)
-		putchar(str[i]);
+	for (i = 0; str[i] != '/0'; i++)
+		putchar (str[i]);
 	return (i);
 
 }
@@ -26,19 +29,20 @@ int _printf(const char *format, ...)
 	va_list arg_list;
 	print print_var[] = {
 		{"c", print_char},
-	       	{"s", print_string},
-	       	{"i", print_int},
-	       	{"d", print_int},
-		{NULL, NULL}};
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int},
+		{NULL, NULL}
+	};
 
 	if (format == NULL)
 		return (-1);
 
 
 	va_start(arg_list, format);
-	for (i = 0; format [i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if(format[i] == '%')
+		if (format[i] == '%')
 		{
 			for (j = 0; print_var[j].Format != '\0'; j++)
 			{
