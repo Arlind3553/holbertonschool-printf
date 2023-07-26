c2851cd2518cf36cb85d695a46ed555cd380c205
Holberton1990 <ersida9019@gmail.com>
2023-07-26 04:11:07 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -3,17 +3,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_char - function to print char
+ * @cha: argument char to print
+ * Return: len of 1
+ */
 int print_char(va_list cha)
 {
 	char ch;
+
 	ch = va_arg(cha, int);
 	_putchar(ch);
 	return (1);
 }
+/**
+ * print_string - function to print string
+ * @string: string to print
+ * Return: len of str
+ */
 int print_string(va_list string)
 {
 	char *str;
+
 	int i = 0;
+
 	str = va_arg(string, char *);
 	if (str == NULL)
 		str = "(null)";
@@ -23,6 +36,45 @@ int print_string(va_list string)
 	}
 	return (i);
 }
+/**
+ * intt - main function
+ * @n:number
+ * @count: number length
+ * Return: (new) if succesful
+ */
+int intt(long int n, int count)
+{
+	int new = count;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -1 * n;
+	}
+	if (n / 10)
+		new = intt(n / 10, count + 1);
+	_putchar(n % 10 + '0');
+	return (new);
+}
+/**
+ * print_int - function that prints int from va_list
+ * @arg_list: argument list
+ * Return: (intt(i, 2)); if succesful
+ */
+int print_int(va_list arg_list)
+{
+	long int i;
+
+	i = va_arg(arg_list, int);
+	if (i < 0)
+		return (intt(i, 2));
+	return (intt(i, 1));
+}
+/**
+ * _printf - print everything
+ * @format: what to print
+ * Return: 0 if succesful
+ */
 int _printf(const char *format, ...)
 {
 	int i = 0, j, count = 0;
@@ -30,6 +82,8 @@ int _printf(const char *format, ...)
 	p_struct print_var[] = {
 	{"c", print_char},
 	{"s", print_string},
+	{"i", print_int},
+	{"d", print_int},
 	{NULL, NULL}};
 	if (format == NULL)
 		return (-1);
