d3949d7eb7b52e0d317052c249d6e51fa0a57447
Arlind3553 <georgetesco@yahoo.com>
2023-07-26 04:27:23 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -8,6 +8,7 @@
  * @cha: argument char to print
  * Return: len of 1
  */
+
 int print_char(va_list cha)
 {
 	char ch;
@@ -16,6 +17,7 @@ int print_char(va_list cha)
 	_putchar(ch);
 	return (1);
 }
+
 /**
  * print_string - function to print string
  * @string: string to print
@@ -24,7 +26,6 @@ int print_char(va_list cha)
 int print_string(va_list string)
 {
 	char *str;
-
 	int i = 0;
 
 	str = va_arg(string, char *);
@@ -36,74 +37,19 @@ int print_string(va_list string)
 	}
 	return (i);
 }
-<<<<<<< HEAD
-/**
- * intt - main function
- * @n:number
- * @count: number length
- * Return: (new) if succesful
- */
-int intt(long int n, int count)
-{
-	int new = count;
 
-=======
-int intt(long int n, int count)
-{
-	int new = count;
->>>>>>> b31998ac4ca9b329a25d34cd99f00608b4f138a5
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -1 * n;
-	}
-	if (n / 10)
-<<<<<<< HEAD
-		new = intt(n / 10, count + 1);
-	_putchar(n % 10 + '0');
-	return (new);
-}
-/**
- * print_int - function that prints int from va_list
- * @arg_list: argument list
- * Return: (intt(i, 2)); if succesful
- */
-int print_int(va_list arg_list)
-{
-	long int i;
-
-=======
-		new = intt(n/10, count + 1);
-	_putchar(n % 10 + '0');
-	return (new);
-}
-int print_int(va_list arg_list)
-{
-	long int i;
->>>>>>> b31998ac4ca9b329a25d34cd99f00608b4f138a5
-	i = va_arg(arg_list, int);
-	if (i < 0)
-		return (intt(i, 2));
-	return (intt(i, 1));
-}
-<<<<<<< HEAD
 /**
  * _printf - print everything
  * @format: what to print
  * Return: 0 if succesful
  */
-=======
->>>>>>> b31998ac4ca9b329a25d34cd99f00608b4f138a5
 int _printf(const char *format, ...)
 {
 	int i = 0, j, count = 0;
 	va_list arg_list;
-	p_struct print_var[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"i", print_int},
-	{"d", print_int},
-	{NULL, NULL}};
+	p_struct print_var[] = {{"c", print_char}, {"s", print_string},
+	{"i", change_int}, {"d", change_int}, {NULL, NULL}};
+
 	if (format == NULL)
 		return (-1);
 	va_start(arg_list, format);
@@ -130,6 +76,7 @@ int _printf(const char *format, ...)
 			else if (format[i + 1] == '\0')
 				return (-1);
 			i++;
+
 		}
 		else
 			count += _putchar(format[i]);
