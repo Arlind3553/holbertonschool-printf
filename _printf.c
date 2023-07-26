60013d6abd9083ad9eb63504ebdf7658efad07ca
Holberton1990 <ersida9019@gmail.com>
2023-07-26 01:51:16 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,39 +1,18 @@
 #include "main.h"
-#include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
 
-
 int print_char(va_list cha)
 {
-<<<<<<< HEAD
-	char ch = va_arg(char_li, int);
-
-	putchar (ch);
-
-	return (1);
-}
-
-int print_string(va_list string_li)
-=======
 	char ch;
-
 	ch = va_arg(cha, int);
 	_putchar(ch);
 	return (1);
 }
-
 int print_string(va_list string)
->>>>>>> 88b8fa6fe4c661385bc4118ae8306e558b145f4c
 {
 	char *str;
 	int i = 0;
-
-<<<<<<< HEAD
-	str = va_arg(string_li, char*);
-	for (i = 0; str[i] != '/0'; i++)
-		putchar (str[i]);
-=======
 	str = va_arg(string, char *);
 	if (str == NULL)
 		str = "(null)";
@@ -41,37 +20,20 @@ int print_string(va_list string)
 	{
 		_putchar(str[i]);
 	}
->>>>>>> 88b8fa6fe4c661385bc4118ae8306e558b145f4c
 	return (i);
 }
-
 int _printf(const char *format, ...)
 {
 	int i = 0, j, count = 0;
 	va_list arg_list;
-<<<<<<< HEAD
-	print print_var[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_int},
-		{NULL, NULL}
-	};
-=======
 	p_struct print_var[] = {
-		{"c", print_char}, 
-		{"s", print_string},
-		{NULL, NULL}};
->>>>>>> 88b8fa6fe4c661385bc4118ae8306e558b145f4c
-
+	{"c", print_char},
+	{"s", print_string},
+	{NULL, NULL}};
 	if (format == NULL)
 		return (-1);
 	va_start(arg_list, format);
-<<<<<<< HEAD
-	for (i = 0; format[i] != '\0'; i++)
-=======
 	for (i = 0; format != NULL && format[i] != '\0'; i++)
->>>>>>> 88b8fa6fe4c661385bc4118ae8306e558b145f4c
 	{
 		if (format[i] == '%')
 		{
@@ -94,7 +56,6 @@ int _printf(const char *format, ...)
 			else if (format[i + 1] == '\0')
 				return (-1);
 			i++;
-
 		}
 		else
 			count += _putchar(format[i]);
